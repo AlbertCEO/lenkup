6a75d469c9216b24263af8a118450b6141ad8577
ALBERT IMASUEN <studentxapp@gmail.com>
2024-03-21 19:47:07 -0400

    Creating the Video media room

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -7,10 +7,13 @@ import { Separator } from "@/components/ui/separator";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { ServerHeader } from "./server-header";
+import { ServerSearch } from "./server-search";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
+import { ServerMember } from "./server-member";
 
 // import { ServerHeader } from "./server-header";
 // import { ServerSearch } from "./server-search";
-// import { ServerSection } from "./server-section";
 // import { ServerChannel } from "./server-channel";
 // import { ServerMember } from "./server-member";
 
@@ -79,7 +82,7 @@ export const ServerSidebar = async ({
       />
       <ScrollArea className="flex-1 px-3">
         <div className="mt-2">
-          {/* <ServerSearch
+          <ServerSearch
             data={[
               {
                 label: "Text Channels",
@@ -118,85 +121,85 @@ export const ServerSidebar = async ({
                 }))
               },
             ]}
-          /> */}
+          />
         </div>
         <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
         {!!textChannels?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="channels"
               channelType={ChannelType.TEXT}
               role={role}
               label="Text Channels"
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {textChannels.map((channel) => (
+              {textChannels.map((channel) => (
                 <ServerChannel
                   key={channel.id}
                   channel={channel}
                   role={role}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
         {!!audioChannels?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="channels"
               channelType={ChannelType.AUDIO}
               role={role}
               label="Voice Channels"
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {audioChannels.map((channel) => (
+              {audioChannels.map((channel) => (
                 <ServerChannel
                   key={channel.id}
                   channel={channel}
                   role={role}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
         {!!videoChannels?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="channels"
               channelType={ChannelType.VIDEO}
               role={role}
               label="Video Channels"
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {videoChannels.map((channel) => (
+              {videoChannels.map((channel) => (
                 <ServerChannel
                   key={channel.id}
                   channel={channel}
                   role={role}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
         {!!members?.length && (
           <div className="mb-2">
-            {/* <ServerSection
+            <ServerSection
               sectionType="members"
               role={role}
               label="Members"
               server={server}
-            /> */}
+            />
             <div className="space-y-[2px]">
-              {/* {members.map((member) => (
+              {members.map((member) => (
                 <ServerMember
                   key={member.id}
                   member={member}
                   server={server}
                 />
-              ))} */}
+              ))}
             </div>
           </div>
         )}
